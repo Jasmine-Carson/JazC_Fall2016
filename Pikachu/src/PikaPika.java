32d68f73fa984cd257f2dffae43a55e88867e2eb
Jazzy <fairy7823@gmail.com>
2016-08-29 08:41:10 -0700

    Pikachu Project!

---
+++ Pikachu/src/PikaPika.java
@@ -0,0 +1,12 @@
+
+public class PikaPika {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("                 (\\__/)");
+		System.out.println("                 (o^.^)");
+		System.out.println("                z(_(\")(\")");
+	}
+
+}
