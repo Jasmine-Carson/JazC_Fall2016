edbdc122fe23e0049cf3d034081a50329a79e718
Jazzy <fairy7823@gmail.com>
2016-09-29 08:41:12 -0700

    Processing Numbers

---
+++ Collaborative Programming/src/ProccessigNumbers.java
@@ -0,0 +1,31 @@
+//Jasmine Carson with Kaitlyn Chan
+import java.util.*;
+public class ProccessigNumbers {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("How many numbers?");
+		int loops = input.nextInt();
+		int next;
+		System.out.println("Enter your numbers separated by spaces. Start with an even number.");
+		int max=input.nextInt();
+		int min=max;
+		int evenmax=max;
+		int evensum = evenmax;
+		for (int i=1; i<loops; i++){
+			next=input.nextInt();
+			if (next<=min){
+				min=next;
+			}
+			else if (next>=max){
+				max=next;
+			}
+			if (next%2==0){
+				evensum=evensum+next;
+				if(next>=evenmax){
+					evenmax=next;
+				}
+			}
+		}
+		System.out.println("Max is "+max+". Min is "+min+". Sum of evens is "+evensum+". Max even is "+evenmax+".");
+	}
+}
