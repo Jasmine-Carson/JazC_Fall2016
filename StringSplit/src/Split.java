fb537a05fcd4e028484db1d5b636793d905502e6
Jazzy <fairy7823@gmail.com>
2016-10-25 09:26:10 -0700

    StringSplit-Spaces

--- StringSplit/src/Split.java
+++ StringSplit/src/Split.java
@@ -7,14 +7,20 @@ public class Split {
 		//It's a method that acts on a string, <StringName>.split(<String sp>);
 		//Where sp is the string where the string splits
 		//And it returns an array
-		// Example: "I like apples!".split(" "); 
+		"I like apples!".split(" "); 
 		//		it will split at spaces and return an array of ["I","like","apples!"]
 		// Example 2: "I really like really red apples"split("really")
 		//		it will split at the word "really" and return an array of ["I "," like "," apples!"]
 		
 		System.out.println(Arrays.toString("I reallyreally like apples".split("really")));
+		System.out.println();
 		sandwichSplit("applespineapplesbreadlettustomatobaconmayohambreadcheese");
 		sandwichSplit("breadcheesetomatobreadmayomeatbread");
+		sandwichSplit("applebreadcheese");
+		System.out.println();
+		sandwichSpaces("apples pineapples bread lettus tomato bacon mayo ham bread cheese");
+		sandwichSpaces("bread cheese tomato bread mayo meat bread");
+		sandwichSpaces("apple bread cheese");
 	}
 		
 		//Your task:
@@ -23,13 +29,20 @@ public class Split {
 		 * What if it's a fancy sandwich with multiple pieces of bread?
 		*/
 	public static void sandwichSplit(String sandwich){
+		if (sandwich.indexOf("bread")<0){
+			System.out.println("This isn't a sandwich.");
+			return;
+		}
 		String breadfinder= sandwich.substring(sandwich.indexOf("bread")+5);
 		sandwich=breadfinder;
+		if (sandwich.indexOf("bread")<0){
+			System.out.println("This isn't a sandwich.");
+			return;
+		}
 		while(breadfinder.indexOf("bread")>=0){
 			breadfinder = breadfinder.substring(breadfinder.indexOf("bread")+5);
 		}
 		String[] filling = sandwich.split("bread");
-		System.out.println(Arrays.toString(filling));
 		if ((breadfinder.trim()).equals("")){
 			for (int i=0; i<filling.length; i++){
 				System.out.print(filling[i]);
@@ -48,7 +61,15 @@ public class Split {
 		 * use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
 		 * Again, what if it's a fancy sandwich with multiple pieces of bread?
 		*/
-
+	public static void sandwichSpaces(String sandwichwithspace){
+		sandwichwithspace=sandwichwithspace.trim();
+		String[] fillingarray = sandwichwithspace.split(" ");
+		String sandwich = ""; 
+		for (int i=0; i<fillingarray.length; i++){
+			sandwich = sandwich+fillingarray[i];
+		}
+		sandwichSplit(sandwich);
+	}
 		
 
 	
